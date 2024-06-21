54bdb37498d98daa0f447db0dd8da20224d7ba6b
KaiqueGovani <54339985+KaiqueGovani@users.noreply.github.com>
2024-06-21 00:16:14 -0300

    [ Nextjs App ] - Finished Frontend Prototype

---
+++ nextjs-app/src/app/pagina1/page.tsx
@@ -0,0 +1,3 @@
+export default function Pagina1Page() {
+  return <p>Pagina 1</p>;
+}
---
+++ nextjs-app/src/components/EventImage.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export type EventImageProps = {
+  src: string;
+  alt: string;
+};
+
+export function EventImage(props: EventImageProps) {
+  return (
+    <Image
+      src={props.src}
+      alt={props.alt}
+      width={277}
+      height={180}
+      priority
+      className="rounded-2xl"
+      objectFit="cover"
+    />
+  );
+}
